45f7ddca8b3a6d3109ad7ac5bc88fd2695ca68bb
cjimd90 <cjimenezd90@gmail.com>
2019-03-18 01:33:49 -0500

    Clase 4 Funciones

---
+++ clase4.js
@@ -0,0 +1,10 @@
+var nombre = 'Cristian', edad = 28
+
+function imprimirEdad(nombre, edad) {
+  console.log(nombre + ' tiene ' + edad)
+}
+
+imprimirEdad(nombre, edad)
+imprimirEdad('Juan', 23)
+imprimirEdad('Alex', 29)
+imprimirEdad()
