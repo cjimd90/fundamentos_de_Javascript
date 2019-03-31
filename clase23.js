2f5a1a02b741d8c053bcb3b739903fffb0cc6779
cjimd90 <cjimenezd90@gmail.com>
2019-03-31 08:45:13 -0500

    Clase 23 This en las arrays function

---
+++ clase23.js
@@ -0,0 +1,19 @@
+function Persona(nombre, apellido, altura) {
+  this.nombre = nombre
+  this.apellido = apellido
+  this.altura = altura
+}
+
+Persona.prototype.saludar = function () {
+  console.log(`Hola, me llamo ${this.nombre} ${this.apellido}`)
+}
+
+Persona.prototype.soyAlto= function () {
+  return this.altura > 1.8
+}
+
+var cristian = new Persona('Cristian', 'Jimenez', 1.82)
+var erika = new Persona('Erika', 'Luna', 1.58)
+var pedro = new Persona('Pedro', 'Martinez', 1.85)
+var patricia = new Persona('Patricia', 'Escobar', 1.62)
+var jaime = new Persona('Jaime', 'Ochoa', 1.81)
