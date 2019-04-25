67b1dd9e60174a7a5ab778f900fd656be0e1ef74
cjimd90 <cjimenezd90@gmail.com>
2019-04-25 09:50:24 -0500

    Clase 43 Cuando usar punto y coma en JavaScript

---
+++ clase43.js
@@ -0,0 +1,19 @@
+//  CASO 1
+
+console.log('Hola mi nombre es Cristian');
+[1, 2, 3].forEach(n => console.log(n * 2))
+
+//  CASO 2
+
+const nombre = 'Cristian'
+console.log('Hola mi nombre es Cristian')
+;`${nombre} es un desarrollador`
+
+//  CASO 3
+
+function calcularDoble(numero) {
+  return {
+    original: numero,
+    doble: numero *2
+  }
+}
